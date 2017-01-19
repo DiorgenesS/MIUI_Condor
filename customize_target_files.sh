bd57c8d1e5bb097133b6b9d8eb80c34d7fc81b77
Diorgenes <dyogenes_gd@hotmail.com>
2017-01-18 22:43:12 -0300

    Added multi cust variants for miui

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -11,3 +11,7 @@ rm -rf out/target_files/SYSTEM/vendor/preinstall
 
 #Added device features
 cp -f other/condor.xml out/target_files/SYSTEM/etc/device_features
+
+#Added multi cust variants for miui
+rm -rf out/target_files/DATA/miui/cust
+cp -rf other/cust out/target_files/DATA/miui
