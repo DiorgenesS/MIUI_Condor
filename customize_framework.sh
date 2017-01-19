2c4bb38a0c7350b4b3fa3e6d99043fad0e3469f3
Diorgenes <dyogenes_gd@hotmail.com>
2017-01-18 22:34:02 -0300

    use "vendor editor"

--- customize_framework.sh
+++ customize_framework.sh
@@ -2,6 +2,12 @@
 # $1: dir for miui
 # $2: dir for original
 
+OUT_MIUI_FRAMEWORK_DIR=$1
+OUT_FRAMEWORK_DIR=$2
+if [ "$OUT_FRAMEWORK_DIR" == "out/framework" ];then
+    cp ${OUT_FRAMEWORK_DIR/out\//}.jar.out/smali/android/widget/Editor*.smali $OUT_FRAMEWORK_DIR/smali/android/widget/
+fi
+
 APKTOOL="$PORT_ROOT/tools/apktool --quiet"
 GIT_APPLY=$PORT_ROOT/tools/git.apply
 BUILD_OUT=out
